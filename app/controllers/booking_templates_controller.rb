3719228f1bb7ea5cfd8527dc48790c708ea772cd
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-07-09 23:31:39 +0200

    Implement BookingTemplate support.

---
+++ app/controllers/booking_templates_controller.rb
@@ -0,0 +1,23 @@
+class BookingTemplatesController < AuthorizedController
+  # Actions
+  def create
+    create! do |success, failure|
+      success.html { redirect_to collection_path }
+    end
+  end
+
+  def update
+    update! do |success, failure|
+      success.html { redirect_to collection_path }
+    end
+  end
+
+  def new_booking
+    booking_params = params[:booking] || {}
+    booking_params[:value_date] ||= Date.today
+    booking_params[:code]       ||= (Booking.maximum(:code) || 0) + 1
+    booking_parameters = @booking_template.booking_parameters(booking_params)
+
+    redirect_to simple_edit_bookings_path(:booking => booking_parameters)
+  end
+end
