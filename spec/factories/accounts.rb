60b3c4a3ac62fa5e37c9249d217a16d97558eac5
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-08-19 09:23:25 +0200

    Update factories for accounts.

--- spec/factories/accounts.rb
+++ spec/factories/accounts.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
     title 'Test Account'
     association :account_type
   end
-  
+
   factory :accounts_payable, :parent => :account do
     code  '2000'
     title 'Accounts Payable'
@@ -14,6 +14,36 @@ FactoryGirl.define do
   factory :cash_account, :parent => :account do
     code  '1000'
     title 'Cash'
+    association :account_type, :factory => :current_assets
+  end
+
+  factory :eft_account, :parent => :account do
+    title 'EFT Kontokorrent'
+    code '1021'
+    association :account_type, :factory => :current_assets
+  end
+
+  factory :service_account, :class => Account do
+    title 'Dienstleistungsertrag'
+    code '3200'
+    association :account_type, :factory => :earnings
+  end
+
+  factory :material_account, :parent => :account do
+    title "Materialaufwand"
+    code  "4000"
+    association :account_type, :factory => :costs
+  end
+
+  factory :credit_account, :class => Account do
+    title "Kreditoren"
+    code  "2000"
+    association :account_type, :factory => :outside_capital
+  end
+
+  factory :debit_account, :class => Account do
+    title 'direct account'
+    code '1100'
     association :account_type, :factory => :current_assets
   end
 end
