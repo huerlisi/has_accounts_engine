46c18f7d2e299c79f393eda953687b3cec83a0c3
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2015-05-29 11:42:50 +0200

    chore(test): unify RSpec config with has_accounts
    We got a deprecation warning in rspec runs.
    
    This patch fixes the deprecation warning regarding spec type inference.
    It also enables transaction fixtures, mostly to stay in sync with
    has_accounts.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -38,4 +38,10 @@ RSpec.configure do |config|
 
   # == Mock Framework
   config.mock_with :rspec
+
+  # Fixtures
+  config.use_transactional_fixtures = true
+
+  # Legacy
+  config.infer_spec_type_from_file_location!
 end
