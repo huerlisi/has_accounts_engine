04199db1408fd1aeb6ff8d6b159a24a61741caec
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2016-01-06 23:22:48 +0100

    chore(bookings): drop support for multiple click actions in booking items
    We did reuse the same booking and template items in the old `new` form
    and to show existing entries. As we got rid of the previous booking
    wizard, we now clean up unused code.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,27 +5,6 @@ class BookingsController < HasAccountsController
   has_scope :by_amount, :using => [:from, :to]
   has_scope :by_date, :using => [:from, :to]
 
-  # Actions
-  def select_booking
-    @booking = Booking.find(params[:id]).dup
-
-    # Clear reference
-    @booking.reference  = nil
-
-    increment_booking_code
-    # Take value date from form
-    @booking.value_date = params[:booking][:value_date]
-
-    render :action => 'edit'
-  end
-
-  def select
-    @booking = Booking.new(params[:booking])
-    increment_booking_code
-    @booking_templates = BookingTemplate.where(:type => [nil, 'BookingTemplate']).where("NOT(code LIKE '%:%')").paginate(:page => params[:page])
-    @bookings = Booking.where("title LIKE ?", '%' + @booking.title + '%').order('value_date DESC').paginate(:page => params[:page])
-  end
-
   def create
     create! { new_booking_path }
   end
